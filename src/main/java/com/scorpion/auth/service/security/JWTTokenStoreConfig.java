ed01e38236f48c55f71e3406eb1d740612972b1b
kyeljmd <kyeljohnmdavid@gmail.com>
2017-05-15 19:51:47 +0800

    Externalize JWT Signing key

--- src/main/java/com/scorpion/auth/service/security/JWTTokenStoreConfig.java
+++ src/main/java/com/scorpion/auth/service/security/JWTTokenStoreConfig.java
@@ -1,5 +1,6 @@
 package com.scorpion.auth.service.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -14,6 +15,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
  */
 @Configuration
 public class JWTTokenStoreConfig {
+
+    @Value("${jwt.signingKey}")
+    private String jwtSigningKey;
 
     @Bean
     public TokenStore tokenStore() {
